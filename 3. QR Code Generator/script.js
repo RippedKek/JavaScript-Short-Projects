c7e14664f6d342a6a85dc26ceae5fe5c9d77654e
RippedKek <tanjeeb1909@gmail.com>
2024-07-05 20:35:15 +0600

    QR Generator

---
+++ 3. QR Code Generator/script.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('.input input');
+const img = document.querySelector('.qr');
+const btn = document.querySelector('.btn');
+const url = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='; 
+
+function generateQR(){
+    const inputURL = input.value;
+    if(inputURL === '') alert('You must enter a URL!');
+    else{
+        img.src = url + inputURL;
+        input.value = '';
+    }
+}
+
+input.addEventListener('keydown', (event)=>{
+    if(event.key === 'Enter')
+        generateQR();
+});
+
+document.load
